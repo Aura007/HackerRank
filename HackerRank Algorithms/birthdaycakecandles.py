b9a1461b01a69260ba13cd3651e0ba40667fac18
Aura007 <87700879+Aura007@users.noreply.github.com>
2024-09-05 22:37:17 +0530

    Add files via upload

---
+++ HackerRank Algorithms/birthdaycakecandles.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    maxi=candles[0]
+    sum1=0
+    for num in candles:
+        if num>maxi:
+            maxi=num
+    for numi in candles :
+        if maxi==numi:
+            sum1=sum1+1
+    return sum1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
